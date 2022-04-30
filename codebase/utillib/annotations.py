116d95ca4c6a05032d3d55a056b797a6e296976d
schoepp <alexandre.schoepp@telecom-sudparis.eu>
2022-04-30 10:32:49 +0200

    fixing the annotation

--- codebase/utillib/annotations.py
+++ codebase/utillib/annotations.py
@@ -4,14 +4,13 @@ import typing
 def on_fail_ask_user_politely(f: typing.Callable):
     def wrapper(*args, **kwargs):
         try:
-            f(*args, **kwargs)
+            return f(*args, **kwargs)
         except Exception as e:
-            print("\033[31;1m Execution failed.\033[0m Do you want to know why ?")
+            print("\n\033[31;1m Execution failed.\033[0m\n\tDo you want to know why ?\n\t")
             if "y" in input().lower():
                 raise e
             else:
-                return
+                return e
         except KeyboardInterrupt:
-            print("\033[31;1mInterrupted\033[0m with success")
-
+            print("\n\033[31;1mInterrupted\033[0m with success\n")
     return wrapper
